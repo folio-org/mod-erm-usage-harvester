ea7e9060e74462f9c20cd6c5147370c4ac349afb
Tino Ryll <ryll@ub.uni-leipzig.de>
2019-01-29 14:59:02 +0100

    Update test && Main-Verticle

--- mod-erm-usage-harvester-core/src/test/java/org/olf/erm/usage/harvester/APITest.java
+++ mod-erm-usage-harvester-core/src/test/java/org/olf/erm/usage/harvester/APITest.java
@@ -27,16 +27,21 @@ public class APITest {
 
   private static Vertx vertx;
 
-  private static final HarvesterVerticle harvester = new HarvesterVerticle();
-
-  private static final String deployCfg = "{\n" + "  \"okapiUrl\": \"http://localhost:9130\",\n"
-      + "  \"tenantsPath\": \"/_/proxy/tenants\",\n" + "  \"reportsPath\": \"/counter-reports\",\n"
-      + "  \"providerPath\": \"/usage-data-providers\",\n"
-      + "  \"aggregatorPath\": \"/aggregator-settings\",\n" + "  \"moduleIds\": [\n"
-      + "    \"mod-erm-usage-0.2.0-SNAPSHOT\",\n"
-      + "    \"mod-erm-usage-harvester-0.2.0-SNAPSHOT\"\n" + "  ],\n"
-      + "  \"loginPath\": \"/bl-users/login\",\n" + "  \"requiredPerm\": \"ermusage.all\"\n" + "}\n"
-      + "";
+  private static final String deployCfg =
+      "{\n"
+          + "  \"okapiUrl\": \"http://localhost:9130\",\n"
+          + "  \"tenantsPath\": \"/_/proxy/tenants\",\n"
+          + "  \"reportsPath\": \"/counter-reports\",\n"
+          + "  \"providerPath\": \"/usage-data-providers\",\n"
+          + "  \"aggregatorPath\": \"/aggregator-settings\",\n"
+          + "  \"moduleIds\": [\n"
+          + "    \"mod-erm-usage-0.2.0-SNAPSHOT\",\n"
+          + "    \"mod-erm-usage-harvester-0.2.0-SNAPSHOT\"\n"
+          + "  ],\n"
+          + "  \"loginPath\": \"/bl-users/login\",\n"
+          + "  \"requiredPerm\": \"ermusage.all\"\n"
+          + "}\n"
+          + "";
 
   @BeforeClass
   public static void setup(TestContext context) {
@@ -46,7 +51,10 @@ public class APITest {
     cfg.put("testing", true);
     cfg.put("http.port", 8082);
     RestAssured.port = 8082;
-    vertx.deployVerticle(harvester, new DeploymentOptions().setConfig(cfg),
+    RestAssured.basePath = "erm-usage-harvester";
+    vertx.deployVerticle(
+        "org.folio.rest.RestVerticle",
+        new DeploymentOptions().setConfig(cfg),
         context.asyncAssertSuccess());
   }
 
@@ -58,14 +66,15 @@ public class APITest {
 
   @Test
   public void startHarvester403() {
-    when().get("/harvester/start").then().statusCode(403);
+    when().get("/start").then().statusCode(403);
   }
 
   @Test
   public void startHarvester200() {
-    given().header(new Header(XOkapiHeaders.TENANT, "testtenant"))
+    given()
+        .header(new Header(XOkapiHeaders.TENANT, "testtenant"))
         .when()
-        .get("/harvester/start")
+        .get("/start")
         .then()
         .statusCode(200)
         .body("message", containsString("testtenant"));
@@ -73,23 +82,28 @@ public class APITest {
 
   @Test
   public void startProvider403() {
-    when().get("/harvester/start/5b8ab2bd-e470-409c-9a6c-845d979da05e").then().statusCode(403);
+    when().get("/start/5b8ab2bd-e470-409c-9a6c-845d979da05e").then().statusCode(403);
   }
 
   @Test
   public void startProvider200() {
-    given().header(new Header(XOkapiHeaders.TENANT, "testtenant"))
+    given()
+        .header(new Header(XOkapiHeaders.TENANT, "testtenant"))
         .when()
-        .get("/harvester/start/5b8ab2bd-e470-409c-9a6c-845d979da05e")
+        .get("/start/5b8ab2bd-e470-409c-9a6c-845d979da05e")
         .then()
         .statusCode(200)
-        .body("message", allOf(containsString("testtenant"),
-            containsString("5b8ab2bd-e470-409c-9a6c-845d979da05e")));
+        .body(
+            "message",
+            allOf(
+                containsString("testtenant"),
+                containsString("5b8ab2bd-e470-409c-9a6c-845d979da05e")));
   }
 
   @Test
   public void getImplementations() {
-    when().get("/harvester/impl")
+    when()
+        .get("/impl")
         .then()
         .statusCode(200)
         .body("implementations.size()", greaterThanOrEqualTo(2))
@@ -98,7 +112,8 @@ public class APITest {
 
   @Test
   public void getImplementationsAggregator() {
-    when().get("/harvester/impl?aggregator=true")
+    when()
+        .get("/impl?aggregator=true")
         .then()
         .statusCode(200)
         .body("implementations.size()", greaterThanOrEqualTo(1))
@@ -108,7 +123,8 @@ public class APITest {
 
   @Test
   public void getImplementationsNonAggregator() {
-    when().get("/harvester/impl?aggregator=false")
+    when()
+        .get("/impl?aggregator=false")
         .then()
         .statusCode(200)
         .body("implementations.size()", greaterThanOrEqualTo(1))
