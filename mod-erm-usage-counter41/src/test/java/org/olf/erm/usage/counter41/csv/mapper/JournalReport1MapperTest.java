313faf264419933a39a6eea255c1b958c37b7412
Tino Ryll <ryll@ub.uni-leipzig.de>
2019-02-18 10:29:37 +0100

    Fix static date in test data and remove unused imports

--- mod-erm-usage-counter41/src/test/java/org/olf/erm/usage/counter41/csv/mapper/JournalReport1MapperTest.java
+++ mod-erm-usage-counter41/src/test/java/org/olf/erm/usage/counter41/csv/mapper/JournalReport1MapperTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.time.LocalDate;
 import javax.xml.bind.JAXB;
 import org.junit.Test;
 import org.niso.schemas.counter.Report;
@@ -16,8 +17,8 @@ public class JournalReport1MapperTest {
   public void testC4Json() throws URISyntaxException, IOException {
     File file = new File(Resources.getResource("JR1/NSSReport2018-01-2018-03.xml").toURI());
     String expected =
-        new String(
-            Resources.toByteArray(Resources.getResource("JR1/NSSReport2018-01-2018-03.csv")));
+        new String(Resources.toByteArray(Resources.getResource("JR1/NSSReport2018-01-2018-03.csv")))
+            .replace("$$$date_run$$$", LocalDate.now().toString());
     Report report =
         JAXB.unmarshal(file, CounterReportResponse.class).getReport().getReport().get(0);
     JournalReport1Mapper journalReport1Mapper = new JournalReport1Mapper(report);
