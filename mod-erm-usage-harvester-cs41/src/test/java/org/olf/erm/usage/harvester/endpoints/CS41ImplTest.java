5fce50a16954f173d007d56ae59d4d5e3e34ced7
durschtnase <durschtnase@googlemail.com>
2019-03-17 18:52:56 +0100

    Add tests for proxy usage

--- mod-erm-usage-harvester-cs41/src/test/java/org/olf/erm/usage/harvester/endpoints/CS41ImplTest.java
+++ mod-erm-usage-harvester-cs41/src/test/java/org/olf/erm/usage/harvester/endpoints/CS41ImplTest.java
@@ -1,6 +1,11 @@
 package org.olf.erm.usage.harvester.endpoints;
 
-import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.any;
+import static com.github.tomakehurst.wiremock.client.WireMock.anyUrl;
+import static com.github.tomakehurst.wiremock.client.WireMock.post;
+import static com.github.tomakehurst.wiremock.client.WireMock.postRequestedFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlPathEqualTo;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -8,51 +13,66 @@ import com.github.tomakehurst.wiremock.junit.WireMockRule;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.Proxy.Type;
+import java.net.ProxySelector;
+import java.net.SocketAddress;
+import java.net.URI;
+import java.util.Collections;
+import java.util.List;
 import org.folio.rest.jaxrs.model.HarvestingConfig;
 import org.folio.rest.jaxrs.model.SushiConfig;
 import org.folio.rest.jaxrs.model.SushiCredentials;
 import org.folio.rest.jaxrs.model.UsageDataProvider;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import sun.net.spi.DefaultProxySelector;
 
 @RunWith(VertxUnitRunner.class)
 public class CS41ImplTest {
 
-  public static final String SUSHI_SERVICE = "/sushiService";
+  private static final String SUSHI_SERVICE = "/sushiService";
+  private static final String REPORT_TYPE = "JR1";
+  private static final String BEGIN_DATE = "2018-01-01";
+  private static final String END_DATE = "2018-01-31";
   @Rule public WireMockRule wireMockRule = new WireMockRule(wireMockConfig().dynamicPort());
+  @Rule public WireMockRule wireMockProxyRule = new WireMockRule(wireMockConfig().dynamicPort());
+
+  private UsageDataProvider provider =
+      new UsageDataProvider()
+          .withId("67339c41-a3a7-4d19-83e2-c808ab99c8fe")
+          .withHarvestingConfig(
+              new HarvestingConfig().withReportRelease(4).withSushiConfig(new SushiConfig()))
+          .withSushiCredentials(
+              new SushiCredentials().withRequestorId("reqId1").withCustomerId("custId1"));
+
+  @Before
+  public void setup() {
+    this.provider
+        .getHarvestingConfig()
+        .getSushiConfig()
+        .setServiceUrl("http://localhost:" + wireMockRule.port() + SUSHI_SERVICE);
+    ProxySelector.setDefault(new DefaultProxySelector());
+  }
 
   @Test
   public void fetchSingleReport(TestContext ctx) {
-    UsageDataProvider provider =
-        new UsageDataProvider()
-            .withId("67339c41-a3a7-4d19-83e2-c808ab99c8fe")
-            .withHarvestingConfig(
-                new HarvestingConfig()
-                    .withReportRelease(4)
-                    .withSushiConfig(
-                        new SushiConfig()
-                            .withServiceUrl(
-                                "http://localhost:" + wireMockRule.port() + SUSHI_SERVICE)))
-            .withSushiCredentials(
-                new SushiCredentials().withRequestorId("reqId1").withCustomerId("custId1"));
-
     CS41Impl cs41 = new CS41Impl(provider);
 
-    stubFor(
+    wireMockRule.stubFor(
         post(urlPathEqualTo(SUSHI_SERVICE))
-            .willReturn(
-                aResponse()
-                    .withStatus(200)
-                    // .withBodyFile("response1.xml")
-                    .withBodyFile("response1.xml")));
+            .willReturn(aResponse().withStatus(200).withBodyFile("response1.xml")));
 
     Async async = ctx.async();
-    cs41.fetchSingleReport("JR1", "2018-01-01", "2018-01-31")
+    cs41.fetchSingleReport(REPORT_TYPE, BEGIN_DATE, END_DATE)
         .setHandler(
             ar -> {
               assertThat(ar.succeeded()).isTrue();
-              verify(1, postRequestedFor(urlPathEqualTo(SUSHI_SERVICE)));
+              wireMockRule.verify(1, postRequestedFor(urlPathEqualTo(SUSHI_SERVICE)));
 
               async.complete();
             });
@@ -61,12 +81,40 @@ public class CS41ImplTest {
     wireMockRule.stop();
 
     Async async2 = ctx.async();
-    cs41.fetchSingleReport("JR1", "2018-01-01", "2018-01-31")
+    cs41.fetchSingleReport(REPORT_TYPE, BEGIN_DATE, END_DATE)
         .setHandler(
             ar -> {
               assertThat(ar.failed()).isTrue();
               assertThat(ar.cause().getMessage()).contains("Error getting report");
               async2.complete();
             });
+  }
+
+  @Test
+  public void testProxy(TestContext context) {
+    ProxySelector.setDefault(
+        new ProxySelector() {
+          @Override
+          public List<Proxy> select(URI uri) {
+            return Collections.singletonList(
+                new Proxy(Type.HTTP, new InetSocketAddress("localhost", wireMockProxyRule.port())));
+          }
+
+          @Override
+          public void connectFailed(URI uri, SocketAddress sa, IOException ioe) {}
+        });
+
+    final ServiceEndpoint sep = new CS41Impl(provider);
+
+    wireMockRule.stubFor(any(anyUrl()).willReturn(aResponse().withStatus(404)));
+    wireMockProxyRule.stubFor(any(anyUrl()).willReturn(aResponse().withStatus(404)));
+
+    Async async = context.async();
+    sep.fetchSingleReport(REPORT_TYPE, BEGIN_DATE, END_DATE).setHandler(ar -> async.complete());
+
+    async.await(2000);
+
+    wireMockRule.verify(0, postRequestedFor(anyUrl()));
+    wireMockProxyRule.verify(1, postRequestedFor(anyUrl()));
   }
 }
--- mod-erm-usage-harvester-nss/src/test/java/org/olf/erm/usage/harvester/endpoints/NSSTest.java
+++ mod-erm-usage-harvester-nss/src/test/java/org/olf/erm/usage/harvester/endpoints/NSSTest.java
@@ -1,8 +1,10 @@
 package org.olf.erm.usage.harvester.endpoints;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.any;
+import static com.github.tomakehurst.wiremock.client.WireMock.anyUrl;
 import static com.github.tomakehurst.wiremock.client.WireMock.get;
-import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.getRequestedFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -13,30 +15,38 @@ import com.google.common.io.Resources;
 import io.vertx.core.Future;
 import io.vertx.ext.unit.Async;
 import io.vertx.ext.unit.TestContext;
-import io.vertx.ext.unit.junit.RunTestOnContext;
+import io.vertx.ext.unit.junit.Timeout;
 import io.vertx.ext.unit.junit.VertxUnitRunner;
 import java.io.File;
 import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Proxy;
+import java.net.Proxy.Type;
+import java.net.ProxySelector;
+import java.net.SocketAddress;
+import java.net.URI;
+import java.util.Collections;
+import java.util.List;
 import javax.xml.bind.JAXB;
 import org.folio.rest.jaxrs.model.AggregatorSetting;
 import org.folio.rest.jaxrs.model.UsageDataProvider;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.rules.Timeout;
 import org.junit.runner.RunWith;
 import org.niso.schemas.counter.Report;
 import org.niso.schemas.sushi.counter.CounterReportResponse;
 import org.olf.erm.usage.counter41.Counter4Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import sun.net.spi.DefaultProxySelector;
 
 @RunWith(VertxUnitRunner.class)
 public class NSSTest {
 
   @Rule public Timeout timeoutRule = Timeout.seconds(5);
   @Rule public WireMockRule wireMockRule = new WireMockRule(wireMockConfig().dynamicPort());
-  @Rule public RunTestOnContext ctx = new RunTestOnContext();
+  @Rule public WireMockRule wireMockProxyRule = new WireMockRule(wireMockConfig().dynamicPort());
 
   private static final Logger LOG = LoggerFactory.getLogger(NSSTest.class);
   private UsageDataProvider provider;
@@ -60,16 +70,16 @@ public class NSSTest {
                 AggregatorSetting.class)
             .withServiceUrl(wireMockRule.url("mockedAPI"));
     LOG.info("Setting Aggregator URL to: " + aggregator.getServiceUrl());
+    ProxySelector.setDefault(new DefaultProxySelector());
   }
 
   @Test
   public void fetchSingleReportWithAggregatorValidReport(TestContext context) {
-
-    final ServiceEndpoint sep = ServiceEndpoint.create(provider, aggregator);
-    final String url = ((NSS) sep).buildURL(reportType, beginDate, endDate);
+    final NSS sep = new NSS(provider, aggregator);
+    final String url = sep.buildURL(reportType, beginDate, endDate);
 
     LOG.info("Creating stub for: " + url);
-    stubFor(
+    wireMockRule.stubFor(
         get(urlEqualTo(url.replaceAll(wireMockRule.url(""), "/")))
             .willReturn(aResponse().withBodyFile("nss-report-2016-03.xml")));
 
@@ -99,11 +109,11 @@ public class NSSTest {
 
   @Test
   public void fetchSingleReportWithAggregatorInvalidReport(TestContext context) {
-    final ServiceEndpoint sep = ServiceEndpoint.create(provider, aggregator);
-    final String url = ((NSS) sep).buildURL(reportType, beginDate, endDate);
+    final NSS sep = new NSS(provider, aggregator);
+    final String url = sep.buildURL(reportType, beginDate, endDate);
 
     LOG.info("Creating stub for: " + url);
-    stubFor(
+    wireMockRule.stubFor(
         get(urlEqualTo(url.replaceAll(wireMockRule.url(""), "/")))
             .willReturn(aResponse().withBodyFile("nss-report-2018-03-fail.xml")));
 
@@ -124,11 +134,11 @@ public class NSSTest {
 
   @Test
   public void fetchSingleReportWithAggregatorInvalidResponse(TestContext context) {
-    final ServiceEndpoint sep = ServiceEndpoint.create(provider, aggregator);
-    final String url = ((NSS) sep).buildURL(reportType, beginDate, endDate);
+    final NSS sep = new NSS(provider, aggregator);
+    final String url = sep.buildURL(reportType, beginDate, endDate);
 
     LOG.info("Creating stub for: " + url);
-    stubFor(
+    wireMockRule.stubFor(
         get(urlEqualTo(url.replaceAll(wireMockRule.url(""), "/")))
             .willReturn(aResponse().withStatus(404)));
 
@@ -148,7 +158,7 @@ public class NSSTest {
 
   @Test
   public void fetchSingleReportWithAggregatorNoService(TestContext context) {
-    final ServiceEndpoint sep = ServiceEndpoint.create(provider, aggregator);
+    final NSS sep = new NSS(provider, aggregator);
 
     wireMockRule.stop();
 
@@ -176,5 +186,33 @@ public class NSSTest {
             CounterReportResponse.class);
     assertThat(Counter4Utils.getExceptions(reportValid)).isEmpty();
     assertThat(Counter4Utils.getExceptions(reportInvalid)).isNotEmpty();
+  }
+
+  @Test
+  public void testProxy(TestContext context) {
+    ProxySelector.setDefault(
+        new ProxySelector() {
+          @Override
+          public List<Proxy> select(URI uri) {
+            return Collections.singletonList(
+                new Proxy(Type.HTTP, new InetSocketAddress("localhost", wireMockProxyRule.port())));
+          }
+
+          @Override
+          public void connectFailed(URI uri, SocketAddress sa, IOException ioe) {}
+        });
+
+    final NSS sep = new NSS(provider, aggregator);
+
+    wireMockRule.stubFor(any(anyUrl()).willReturn(aResponse().withStatus(404)));
+    wireMockProxyRule.stubFor(any(anyUrl()).willReturn(aResponse().withStatus(404)));
+
+    Async async = context.async();
+    sep.fetchSingleReport(reportType, beginDate, endDate).setHandler(ar -> async.complete());
+
+    async.await(2000);
+
+    wireMockRule.verify(0, getRequestedFor(anyUrl()));
+    wireMockProxyRule.verify(1, getRequestedFor(anyUrl()));
   }
 }
