354a0e62b1cb564b181726b61daf611603b9204c
durschtnase <durschtnase@googlemail.com>
2020-03-29 19:47:56 +0200

    Unify log messages

--- mod-erm-usage-harvester-core/src/main/java/org/olf/erm/usage/harvester/Messages.java
+++ mod-erm-usage-harvester-core/src/main/java/org/olf/erm/usage/harvester/Messages.java
@@ -1,9 +1,31 @@
 package org.olf.erm.usage.harvester;
 
+import org.slf4j.helpers.MessageFormatter;
+
 public class Messages {
 
   static final String ERR_MSG_STATUS = "Received status code %s, %s from %s";
   static final String ERR_MSG_DECODE = "Error decoding response from %s, %s";
+
+  public static String createMsgStatus(int statusCode, String statusMessage, String url) {
+    return String.format(ERR_MSG_STATUS, statusCode, statusMessage, url);
+  }
+
+  public static String createErrMsgDecode(String url, String message) {
+    return String.format(ERR_MSG_DECODE, url, message);
+  }
+
+  public static String createTenantMsg(String tenant, String pattern, Object... args) {
+    return String.format("Tenant: %s, %s", tenant, format(pattern, args));
+  }
+
+  public static String createProviderMsg(String provider, String pattern, Object... args) {
+    return String.format("Provider: %s, %s", provider, format(pattern, args));
+  }
+
+  public static String format(String pattern, Object... args) {
+    return MessageFormatter.arrayFormat(pattern, args).getMessage();
+  }
 
   private Messages() {}
 }
