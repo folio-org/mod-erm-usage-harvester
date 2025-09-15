e7e2961dc83fcb5c6e9a1acba8b878bf1485a0b4
alb3rtino <alb3rtino@users.noreply.github.com>
2025-09-15 12:10:13 +0200

    [MODEUSHARV-144] Fix migration to mod-settings (#202)
    * Replace optional `configuration` interface with `settings`
    
    * Fix permissions for mod-settings
    
    * Fix mod-settings `scope` and `key` values

--- mod-erm-usage-harvester-core/src/main/java/org/olf/erm/usage/harvester/Constants.java
+++ mod-erm-usage-harvester-core/src/main/java/org/olf/erm/usage/harvester/Constants.java
@@ -2,9 +2,9 @@ package org.olf.erm.usage.harvester;
 
 public class Constants {
 
-  public static final String SETTINGS_SCOPE_HARVESTER = "ERM-USAGE-HARVESTER";
-  public static final String SETTINGS_KEY_DAYS_TO_KEEP_LOGS = "daysToKeepLogs";
-  public static final String SETTINGS_KEY_MAX_FAILED_ATTEMPTS = "maxFailedAttempts";
+  public static final String SETTINGS_SCOPE_HARVESTER = "mod-erm-usage-harvester";
+  public static final String SETTINGS_KEY_DAYS_TO_KEEP_LOGS = "days_to_keep_logs";
+  public static final String SETTINGS_KEY_MAX_FAILED_ATTEMPTS = "max_failed_attempts";
   public static final int DEFAULT_DAYS_TO_KEEP_LOGS = 60;
   public static final int DEFAULT_MAX_FAILED_ATTEMPTS = 5;
 
